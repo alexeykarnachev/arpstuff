109faf8db15edd2897c8c562d998e1990a6fc3e3
Alexey Karnachev <alekseykarnachev@gmail.com>
2023-03-07 04:00:53 +0400

    obtain gateway mac via arp

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <net/if.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <netinet/if_ether.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
 
+typedef struct in_addr in_addr;
+typedef struct ifreq ifreq;
+typedef struct ether_arp ether_arp;
 
-void get_mac(unsigned char* mac, char* if_name) {
+ifreq get_ifr(char* if_name) {
     struct ifreq ifr;
     strncpy(ifr.ifr_name, if_name, IFNAMSIZ - 1);
 
@@ -16,65 +23,151 @@ void get_mac(unsigned char* mac, char* if_name) {
     ioctl(sockfd, SIOCGIFHWADDR, &ifr);
     close(sockfd);
 
-    memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
+    return ifr;
 }
+
+// void get_ip_mac(unsigned char* mac, )
 
 void get_mac_str(char* mac_str, unsigned char* mac) {
     sprintf(
         mac_str,
         "%02x:%02x:%02x:%02x:%02x:%02x",
-        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+        mac[0],
+        mac[1],
+        mac[2],
+        mac[3],
+        mac[4],
+        mac[5]
     );
 }
 
-uint32_t get_gateway(const char *if_name)
-{
-	FILE *f = fopen("/proc/net/route", "r");
-	char buf[1024];
-	do {
+in_addr get_gateway_in_addr(const char* if_name) {
+    FILE* f = fopen("/proc/net/route", "r");
+    char buf[1024];
+    do {
         if (fgets(buf, 1024, f) == NULL) {
-            fprintf(stderr, "ERROR: Can't find '%s' interface in the route file\n", if_name);
+            fprintf(
+                stderr,
+                "ERROR: Can't find '%s' if in the route file\n",
+                if_name
+            );
             exit(1);
         }
 
-	} while (strncmp(if_name, buf, strlen(if_name)));
+    } while (strncmp(if_name, buf, strlen(if_name)));
 
-	char* seps = " \t";
-	char *p = strtok(buf, seps);
-	p = strtok(NULL, seps);
-	p = strtok(NULL, seps);
-	p[8] = '\0';
+    char* seps = " \t";
+    char* p = strtok(buf, seps);
+    p = strtok(NULL, seps);
+    p = strtok(NULL, seps);
+    p[8] = '\0';
 
-	uint32_t ip = strtol(p, NULL, 16);
+    uint32_t ip = strtol(p, NULL, 16);
+    struct in_addr addr = {.s_addr = ip};
 
-	fclose(f);
-	return ip;
+    fclose(f);
+    return addr;
 }
 
-uint32_t get_ipv4_str(char* ip_str, uint32_t ip) {
-    sprintf(
-        ip_str,
-        "%d.%d.%d.%d",
-        ((unsigned char*)&ip)[0],
-        ((unsigned char*)&ip)[1],
-        ((unsigned char*)&ip)[2],
-        ((unsigned char*)&ip)[3]
-    );
+// void get_ifr_hwaddr(int fd, struct ifreq *ifr) {
+// }
+
+ether_arp request_mac(ifreq* ifr, uint32_t ip_addr) {
+    /* get arp socket */
+    int fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_ARP));
+	if (fd == -1) {
+        perror("socket");
+        exit(1);
+	}
+
+    /* will be sent to everyone */
+	const unsigned char ether_broadcast_addr[] =
+	{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+
+	if (ioctl(fd, SIOCGIFINDEX, ifr) == -1) {
+        perror("ioctl");
+        exit(1);
+	}
+
+	/* special socket address type used for AF_PACKET */
+	struct sockaddr_ll addr = {0};
+	addr.sll_family   = AF_PACKET;
+	addr.sll_ifindex  = ifr->ifr_ifindex;
+	addr.sll_halen    = ETHER_ADDR_LEN;
+	addr.sll_protocol = htons(ETH_P_ARP);
+	memcpy(addr.sll_addr, ether_broadcast_addr, ETHER_ADDR_LEN);
+
+	/* construct the ARP request */
+    struct ether_arp req;
+	req.arp_hrd = htons(ARPHRD_ETHER);
+	req.arp_pro = htons(ETH_P_IP);
+	req.arp_hln = ETHER_ADDR_LEN;
+	req.arp_pln = sizeof(in_addr_t);
+	req.arp_op  = htons(ARPOP_REQUEST);
+
+	/* zero because that's what we're asking for */
+	memset(&req.arp_tha, 0, sizeof(req.arp_tha));
+	memcpy(&req.arp_tpa, &ip_addr, sizeof(req.arp_tpa));
+
+    if (ioctl(fd, SIOCGIFADDR, ifr) == -1) {
+        perror("ioctl");
+        exit(1);
+	}
+
+	memcpy(&req.arp_sha, (unsigned char *) ifr->ifr_hwaddr.sa_data, sizeof(req.arp_sha));
+	// memcpy(&req.arp_spa, (unsigned char *) ifr->ifr_addr.sa_data + 2, sizeof(req.arp_spa));
+
+	/* actually send it */
+	if (sendto(fd, &req, sizeof(struct ether_arp), 0, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+        perror("sendto");
+        exit(1);
+	}
+
+	while (1) {
+		/* can't use recvfrom() -- no network layer */
+		int len = recv(fd, &req, sizeof(struct ether_arp), 0);
+		if (len == -1) {
+            perror("recv");
+            exit(1);
+		}
+		if (len == 0) {   /* no response */
+			continue;
+		}
+
+		unsigned int from_addr =
+			(req.arp_spa[3] << 24)
+		      | (req.arp_spa[2] << 16)
+		      | (req.arp_spa[1] << 8)
+		      | (req.arp_spa[0] << 0);
+		if (from_addr != ip_addr) {
+			continue;
+		}
+
+		break;
+	}
+
+    return req;
 }
 
 int main() {
     char* if_name = "wlp1s0";
+    ifreq ifr = get_ifr(if_name);
 
-    unsigned char mac[6];
-    char mac_str[18];
-    get_mac(mac, if_name);
-    get_mac_str(mac_str, mac);
+    char my_mac_str[18];
+    get_mac_str(my_mac_str, ifr.ifr_hwaddr.sa_data);
 
-    char gateway_str[18];
-    uint32_t gateway = get_gateway(if_name);
-    get_ipv4_str(gateway_str, gateway);
+    char gateway_ip_str[18];
+    in_addr gateway_in_addr = get_gateway_in_addr(if_name);
+    inet_ntop(AF_INET, &gateway_in_addr, gateway_ip_str, INET_ADDRSTRLEN);
 
-    printf("mac: %s\n", mac_str);
-    printf("gateway: %s\n", gateway_str);
+
+    char gateway_mac_str[18];
+    ether_arp arpr = request_mac(&ifr, gateway_in_addr.s_addr);
+    get_mac_str(gateway_mac_str, arpr.arp_sha);
+
+    printf("my mac: %s\n", my_mac_str);
+    printf("gateway ip: %s\n", gateway_ip_str);
+    printf("gateway mac: %s\n", gateway_mac_str);
+
     return 0;
 }
---
+++ src/main1.c
@@ -0,0 +1,121 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int BROADCAST_PORT = 1900;
+
+void broadcast_message(char* message) {
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    int broadcast = 1;
+    setsockopt(
+        sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)
+    );
+
+    // Set up the broadcast address
+    struct sockaddr_in broadcast_addr;
+    memset(&broadcast_addr, 0, sizeof(broadcast_addr));
+    broadcast_addr.sin_family = AF_INET;
+    broadcast_addr.sin_port = htons(BROADCAST_PORT);
+    broadcast_addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    // Send the message to the broadcast address
+    int send_res = sendto(
+        sock,
+        message,
+        strlen(message),
+        0,
+        (struct sockaddr*)&broadcast_addr,
+        sizeof(broadcast_addr)
+    );
+
+    if (send_res < 0) {
+        perror("sendto");
+        exit(1);
+    }
+
+    close(sock);
+}
+
+int receive_responses(
+    char* message, char** responses, int max_responses
+) {
+    socklen_t addr_len;
+
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_in local_addr;
+    memset(&local_addr, 0, sizeof(local_addr));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(BROADCAST_PORT);
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    int bind_res = bind(
+        sock, (struct sockaddr*)&local_addr, sizeof(local_addr)
+    );
+    if (bind_res < 0) {
+        perror("bind");
+        close(sock);
+        exit(1);
+    }
+
+    // Set a timeout on the socket so we don't block forever
+    // struct timeval tv = {.tv_sec = 5, .tv_usec = 0};
+    // if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+    //     perror("setsockopt");
+    //     close(sock);
+    //     exit(1);
+    // }
+
+    // Receive responses until we reach the maximum number or timeout
+    int num_responses = 0;
+    char buffer[1024];
+    struct sockaddr_in remote_addr;
+    while (num_responses < max_responses) {
+        printf("%d/%d\n", num_responses, max_responses);
+        addr_len = sizeof(remote_addr);
+        memset(buffer, 0, sizeof(buffer));
+        int num_bytes = recvfrom(
+            sock,
+            buffer,
+            1024 - 1,
+            0,
+            (struct sockaddr*)&remote_addr,
+            &addr_len
+        );
+        if (num_bytes < 0) {
+            // Timeout or error occurred, stop receiving
+            break;
+        } else {
+            // Check if the message matches our broadcast message
+            if (strncmp(buffer, message, strlen(message)) == 0) {
+                // Add the remote address to our list of responses
+                char* response = inet_ntoa(remote_addr.sin_addr);
+                responses[num_responses++] = strdup(response);
+            }
+        }
+    }
+
+    close(sock);
+
+    return num_responses;
+}
+
+void main(void) {
+    char* message = "HELLO WORLD HELLO WORLD";
+
+    broadcast_message(message);
+    char* responses[128];
+    int n_responses = receive_responses(message, responses, 128);
+    printf("%d\n", n_responses);
+}
