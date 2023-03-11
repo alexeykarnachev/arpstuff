b12af7f9d2953cb2ea0cf5418530d3386cba216a
Alexey Karnachev <alekseykarnachev@gmail.com>
2023-03-11 05:17:13 +0400

    add reset iptables sh script

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
-echo 1 > /proc/sys/net/ipv4/ip_forward
+./reset_iptables.sh
 
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
 iptables -A FORWARD -j ACCEPT
 iptables -t nat -s 192.168.0.1/24 -A POSTROUTING -j MASQUERADE
